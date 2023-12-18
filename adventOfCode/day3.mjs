857d512554b3e0425036fc530e87552b5390be6b
Laura Richards <lionheartnz85@gmail.com>
2023-12-18 22:41:51 +1300

    wip

--- adventOfCode/day3.mjs
+++ adventOfCode/day3.mjs
@@ -5,7 +5,7 @@
 
 // split at \n
 // [ 0, 1, 2].map((line, index) => function(line, index))
-
+export { findAnswer }
 const sampleData = `467..114..
 ...*......
 ..35..633.
@@ -19,7 +19,9 @@ const sampleData = `467..114..
 
 const separatedData = sampleData.split('\n')
 
-function getIndexes(line, lineIndex) {
+//function to find and add the valid numbers in each line
+//still need to address what if index is 0 for prior and next index -1 lines
+function getValidNumbers(line, lineIndex) {
   const lineArray = line.split('')
   let lineIndexes = []
   const finalLineArray = []
@@ -35,18 +37,111 @@ function getIndexes(line, lineIndex) {
   //   return finalLineArray
   const priorLine = separatedData[lineIndex - 1]
   const nextLine = separatedData[lineIndex + 1]
-
+  const validIndexes = []
   finalLineArray.map((array) => {
     array.map((index) => {
-      if (
-        priorLine[index - 1] &&
-        priorLine[index - 1] !== '.' &&
-        isNaN(priorLine[index - 1])
-      ) {
-        return true
+      if (lineIndex !== 0) {
+        if (validIndexes.includes(array)) {
+          return
+        }
+        if (
+          priorLine[index - 1] &&
+          priorLine[index - 1] !== '.' &&
+          isNaN(Number(priorLine[index - 1]))
+        ) {
+          console.log(priorLine[index - 1], index, 1)
+          validIndexes.push(array)
+          return
+        }
+        if (
+          priorLine[index] &&
+          priorLine[index] !== '.' &&
+          isNaN(Number(priorLine[index]))
+        ) {
+          console.log(priorLine[index], index, 2)
+          validIndexes.push(array)
+          return
+        }
+        if (
+          priorLine[index + 1] &&
+          priorLine[index + 1] !== '.' &&
+          isNaN(Number(priorLine[index + 1]))
+        ) {
+          console.log(priorLine[index + 1], index)
+          validIndexes.push(array)
+          return
+        }
+      }
+      if (nextLine !== undefined) {
+        if (
+          nextLine[index - 1] &&
+          nextLine[index - 1] !== '.' &&
+          isNaN(Number(nextLine[index - 1]))
+        ) {
+          console.log(nextLine[index - 1], index, 4)
+          validIndexes.push(array)
+          return
+        }
+        if (
+          nextLine[index] &&
+          nextLine[index] !== '.' &&
+          isNaN(Number(nextLine[index]))
+        ) {
+          console.log(nextLine[index], index, 5)
+          validIndexes.push(array)
+          return
+        }
+        if (
+          nextLine[index + 1] &&
+          nextLine[index + 1] !== '.' &&
+          isNaN(Number(nextLine[index + 1]))
+        ) {
+          console.log(nextLine[index + 1], index, 6)
+          validIndexes.push(array)
+          return
+        }
+        if (
+          lineArray[index - 1] &&
+          lineArray[index - 1] !== '.' &&
+          isNaN(Number(lineArray[index - 1]))
+        ) {
+          console.log(lineArray[index - 1], index, 7)
+          validIndexes.push(array)
+          return
+        }
+        if (
+          lineArray[index + 1] &&
+          lineArray[index + 1] !== '.' &&
+          isNaN(Number(lineArray[index + 1]))
+        ) {
+          console.log(lineArray[index + 1], index, 8)
+          validIndexes.push(array)
+          return
+        }
       }
     })
   })
+  const validNumbers = []
+  validIndexes.map((array) => {
+    let combinedNumber = 0
+    for (let i = 0; i < array.length; i++) {
+      combinedNumber += lineArray[array[i]]
+    }
+    validNumbers.push(Number(combinedNumber))
+  })
+  console.log(validNumbers)
+  const numSum = validNumbers.reduce((acc, curr) => (acc += curr), 0)
+  return numSum
 }
 
-// console.log(getAnswer(separatedData[0], 0))
+//function to call other function on the data and add it up
+function findAnswer(data) {
+  const separatedData2 = data.split('\n')
+  let total = 0
+  separatedData2.map((line, index) => {
+    total += getValidNumbers(line, index)
+  })
+  return total
+}
+
+// console.log(getIndexes(separatedData[6], 6))
