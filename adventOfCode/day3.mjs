a98a967a5da15cffe2ac763c590a4f9edc470bbb
AdrianNich <aj.nicholas@gmail.com>
2023-12-18 21:57:53 +1300

    added pseudo code for day 3

---
+++ adventOfCode/day3.mjs
@@ -0,0 +1,7 @@
+// step1: find a digit
+// step2: get index of said digit, push that index into an array
+// step3: check if index +1 is also a digit, if so push that index to array, check if that index plus 1 is also a digit, repeat until it is not a digit.
+// step4:
+
+// split at \n
+// [ 0, 1, 2].map((line, index) => function(line, index))
